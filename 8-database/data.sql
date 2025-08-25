b7c7ac262119ec3de3978fbcb9db0101da69fd0e
Bagas Udi Sahsangka <udibagas@gmail.com>
2025-08-25 13:26:59 +0700

    data.sql

---
+++ 8-database/data.sql
@@ -0,0 +1,419 @@
+insert into
+    "Assets" (
+        name,
+        value,
+        category_id,
+        location_id
+    )
+values (
+        'Uşak Airport',
+        3734964331,
+        2,
+        7
+    );
+
+insert into
+    "Assets" (
+        name,
+        value,
+        category_id,
+        location_id
+    )
+values (
+        'Oakland County International Airport',
+        39686077346,
+        1,
+        7
+    );
+
+insert into
+    "Assets" (
+        name,
+        value,
+        category_id,
+        location_id
+    )
+values (
+        'Desert Rock Airport',
+        31041504883,
+        4,
+        5
+    );
+
+insert into
+    "Assets" (
+        name,
+        value,
+        category_id,
+        location_id
+    )
+values (
+        'Lumbala Airport',
+        38502759765,
+        8,
+        8
+    );
+
+insert into
+    "Assets" (
+        name,
+        value,
+        category_id,
+        location_id
+    )
+values (
+        'Spirit Lake Municipal Airport',
+        82239107142,
+        8,
+        7
+    );
+
+insert into
+    "Assets" (
+        name,
+        value,
+        category_id,
+        location_id
+    )
+values (
+        'Mendi Airport',
+        86989935482,
+        1,
+        1
+    );
+
+insert into
+    "Assets" (
+        name,
+        value,
+        category_id,
+        location_id
+    )
+values (
+        'Batticaloa Airport',
+        42643199646,
+        8,
+        7
+    );
+
+insert into
+    "Assets" (
+        name,
+        value,
+        category_id,
+        location_id
+    )
+values (
+        'St Louis Regional Airport',
+        73910601621,
+        7,
+        1
+    );
+
+insert into
+    "Assets" (
+        name,
+        value,
+        category_id,
+        location_id
+    )
+values (
+        'Orange Airport',
+        22730497053,
+        5,
+        7
+    );
+
+insert into
+    "Assets" (
+        name,
+        value,
+        category_id,
+        location_id
+    )
+values (
+        'Sky Harbor Airport',
+        35401453394,
+        6,
+        6
+    );
+
+insert into
+    "Assets" (
+        name,
+        value,
+        category_id,
+        location_id
+    )
+values (
+        'Zhob Airport',
+        45294242324,
+        5,
+        7
+    );
+
+insert into
+    "Assets" (
+        name,
+        value,
+        category_id,
+        location_id
+    )
+values (
+        'Moroak Airport',
+        76895084064,
+        8,
+        4
+    );
+
+insert into
+    "Assets" (
+        name,
+        value,
+        category_id,
+        location_id
+    )
+values (
+        'Sintang(Susilo) Airport',
+        80083179833,
+        1,
+        7
+    );
+
+insert into
+    "Assets" (
+        name,
+        value,
+        category_id,
+        location_id
+    )
+values (
+        'Akron Fulton International Airport',
+        4056349929,
+        7,
+        2
+    );
+
+insert into
+    "Assets" (
+        name,
+        value,
+        category_id,
+        location_id
+    )
+values (
+        'Fort Mackay / Horizon Airport',
+        73102921993,
+        5,
+        2
+    );
+
+insert into
+    "Assets" (
+        name,
+        value,
+        category_id,
+        location_id
+    )
+values (
+        'Narita International Airport',
+        85644200565,
+        4,
+        8
+    );
+
+insert into
+    "Assets" (
+        name,
+        value,
+        category_id,
+        location_id
+    )
+values (
+        'Hendrik Van Eck Airport',
+        42973655998,
+        3,
+        5
+    );
+
+insert into
+    "Assets" (
+        name,
+        value,
+        category_id,
+        location_id
+    )
+values (
+        'Arak Airport',
+        51160226537,
+        4,
+        8
+    );
+
+insert into
+    "Assets" (
+        name,
+        value,
+        category_id,
+        location_id
+    )
+values (
+        'Corumbá International Airport',
+        64378315388,
+        2,
+        4
+    );
+
+insert into
+    "Assets" (
+        name,
+        value,
+        category_id,
+        location_id
+    )
+values (
+        'Congonhas Airport',
+        70590408695,
+        5,
+        4
+    );
+
+insert into
+    "Assets" (
+        name,
+        value,
+        category_id,
+        location_id
+    )
+values (
+        'Pompano Beach Airpark',
+        64630832874,
+        4,
+        2
+    );
+
+insert into
+    "Assets" (
+        name,
+        value,
+        category_id,
+        location_id
+    )
+values (
+        'April River Airport',
+        52225793556,
+        1,
+        6
+    );
+
+insert into
+    "Assets" (
+        name,
+        value,
+        category_id,
+        location_id
+    )
+values (
+        'Ignace Municipal Airport',
+        53247385950,
+        7,
+        7
+    );
+
+insert into
+    "Assets" (
+        name,
+        value,
+        category_id,
+        location_id
+    )
+values (
+        'Silva Bay Seaplane Base',
+        16863724580,
+        2,
+        6
+    );
+
+insert into
+    "Assets" (
+        name,
+        value,
+        category_id,
+        location_id
+    )
+values (
+        'Beira Airport',
+        59082732002,
+        7,
+        5
+    );
+
+insert into
+    "Assets" (
+        name,
+        value,
+        category_id,
+        location_id
+    )
+values (
+        'Whitecourt Airport',
+        7489936392,
+        1,
+        7
+    );
+
+insert into
+    "Assets" (
+        name,
+        value,
+        category_id,
+        location_id
+    )
+values (
+        'Mostyn Airport',
+        8765216724,
+        7,
+        7
+    );
+
+insert into
+    "Assets" (
+        name,
+        value,
+        category_id,
+        location_id
+    )
+values (
+        'Summerside Airport',
+        16494287263,
+        6,
+        6
+    );
+
+insert into
+    "Assets" (
+        name,
+        value,
+        category_id,
+        location_id
+    )
+values (
+        'Simberi Airport',
+        72006997159,
+        5,
+        6
+    );
+
+insert into
+    "Assets" (
+        name,
+        value,
+        category_id,
+        location_id
+    )
+values (
+        'Orsk Airport',
+        83444864164,
+        6,
+        1
+    );
